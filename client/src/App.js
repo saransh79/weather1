16cbeb85b5574e600e7fcc1880ff56962742aa7e
Saransh Gangwar <saransh7919@gmail.com>
2024-06-10 23:02:15 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import { useEffect, useState } from 'react';
+import axios from "axios";
+
+function App() {
+  const [city, setCity]= useState("");
+  const [data, setData]= useState(null);
+
+  useEffect(()=>{
+    const fetchdata= async ()=>{
+      const {data} = await axios.get(`http://localhost:8000/api/v1/my-weather`);
+      // console.log(data);
+      setData(data);
+  }
+  fetchdata();
+  },[])
+  
+  return (
+    <div >
+      <Navbar
+        city={city}
+        setCity= {setCity}
+        setData= {setData}
+      />
+      <Home 
+        data= {data}
+      />
+    </div>
+  );
+}
+
+export default App;
